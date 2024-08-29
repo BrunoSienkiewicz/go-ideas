19d65a5ae9a0ad372cc7bddbac908d2a34a3e748
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-08-29 17:23:25 +0200

    restructured folders

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HomePage from "./components/Główna";
-import IdeasList from "./components/IdeasList";
-
-function App() {
-  return <IdeasList />;
-}
-
-export default App;
